857cbce6419243939ed8689d0f8b5d0630e70079
zahaer <2685446248@qq.com>
2026-09-20 00:40:44 +0800

    Fix transparent status bar backdrop to match app header

--- android/app/src/main/java/io/github/mikumifa/autouma/MainActivity.java
+++ android/app/src/main/java/io/github/mikumifa/autouma/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.mikumifa.autouma;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -16,6 +17,8 @@ public class MainActivity extends BridgeActivity {
 
         WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         int systemBarColor = Color.parseColor("#dff3eb");
+        getWindow().setBackgroundDrawable(new ColorDrawable(systemBarColor));
+        getWindow().getDecorView().setBackgroundColor(systemBarColor);
         getWindow().setStatusBarColor(systemBarColor);
         getWindow().setNavigationBarColor(systemBarColor);
 
